57d2b756cb8e1a39ce8d519beaf0ca96d7dd0559
Marek Kustosz <markust71@hotmail.com>
2025-07-21 00:15:36 +0200

    Remove unused ConnectionData interface from connection-navigator-store.types.ts

--- src/app/store/connection-navigator-store/connection-navigator-store.types.ts
+++ src/app/store/connection-navigator-store/connection-navigator-store.types.ts
@@ -1,11 +1,5 @@
 import { Connection } from '@/app/prisma.types'
 
-// export interface ConnectionData {
-//   booksy_business_name: string
-//   location_raw: string
-//   booksy_url: string
-// }
-
 export interface NavigatorState {
   index: number
   connection: Connection | null
